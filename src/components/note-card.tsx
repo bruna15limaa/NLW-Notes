7a384d0a72ec0618ad819fb8188d087dfa6fcdcf
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-02-06 22:39:48 -0300

    focus

---
+++ src/components/note-card.tsx
@@ -0,0 +1,16 @@
+export function NoteCard(){
+    return(
+        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 outline-none focus-visible:ring-2 focus-visible:ring-lime-400'>
+        <span className='text-sm font-medium text-slate-300'>
+           há 2 dias
+          </span>
+          <p className='text-sm leading-6 text-slate-400'>
+          O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where...
+
+          Para configurar o plugin, é preciso instalar como abaixo:
+          </p>
+
+          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-transparent pointer-events-none'/>
+      </button>
+    )
+}
